a30e03c03e1d11c788bde302fdd94706872b06e5
jsh <ppm_it@naver.com>
2020-07-21 10:36:24 +0900

    video trim option, code convention fix

--- app/src/main/java/kr/co/jsh/utils/videoUtil/VideoOptions.kt
+++ app/src/main/java/kr/co/jsh/utils/videoUtil/VideoOptions.kt
@@ -1,85 +1,86 @@
 package kr.co.jsh.utils.videoUtil
 
+import android.annotation.SuppressLint
 import android.content.Context
 import android.net.Uri
+import android.provider.MediaStore
 import android.util.Log
 import androidx.core.net.toUri
 import com.github.hiteshsondhi88.libffmpeg.ExecuteBinaryResponseHandler
 import com.github.hiteshsondhi88.libffmpeg.FFmpeg
 import com.github.hiteshsondhi88.libffmpeg.FFmpegLoadBinaryResponseHandler
 import com.github.hiteshsondhi88.libffmpeg.exceptions.FFmpegCommandAlreadyRunningException
+import com.google.android.exoplayer2.offline.Download
 import kr.co.jsh.feature.videoedit.TrimmerContract
 import timber.log.Timber
 import java.text.SimpleDateFormat
 import java.util.*
 
 class VideoOptions(private var ctx: Context) {
-    companion object {
-        const val TAG = "VideoOptions"
-    }
 
+    @SuppressLint("SimpleDateFormat")
     fun trimVideo(startPosition: String, endPosition: String, inputPath: String, listener: TrimmerContract.View) {
         val ff = FFmpeg.getInstance(ctx)
         val date = "${SimpleDateFormat("HH_mm_ss").format(Date(System.currentTimeMillis()))}.mp4"
         val filePath = "/storage/emulated/0/DCIM/$date"
-
         ff.loadBinary(object : FFmpegLoadBinaryResponseHandler {
             override fun onFinish() {
-                Log.e("FFmpegLoad", "onFinish")
+                Timber.e("onFinish")
             }
-
-            //outputPath : 출력 파일 명
             override fun onSuccess() {
-                //Log.e("FFmpegLoad", "onSuccess")
+                Timber.e("onSuccess")
+
+                //val command = arrayOf("-y", "-i", inputPath, "-ss", startPosition, "-to", endPosition, "-c", "copy", outputPath)
                 val command =
                     if(startPosition=="00:0.00")
                         arrayOf("-y", "-i", inputPath,"-vcodec","copy","-acodec","copy","-t", endPosition, filePath)
                     else
                         arrayOf("-y", "-i", inputPath, "-ss", startPosition, "-to", endPosition, "-vcodec","copy","-acodec","copy", filePath)
+
                 Timber.e("startPosition: $startPosition / endPosition: $endPosition")
                 try {
                     ff.execute(command, object : ExecuteBinaryResponseHandler() {
                         override fun onSuccess(message: String?) {
                             super.onSuccess(message)
-                            Log.e(TAG, "onSuccess: " + message!!)
+                            Timber.e("onSuccess: ${message!!}")
                         }
 
                         override fun onProgress(message: String?) {
                             super.onProgress(message)
-                            Log.e(TAG, "onProgress: " + message!!)
+                            Timber.e("onProgress: ${message!!}")
                         }
 
                         override fun onFailure(message: String?) {
                             super.onFailure(message)
-                            listener?.onError(message.toString())
-                            Log.e(TAG, "onFailure: " + message!!)
+                            listener.onError(message.toString())
+                            Timber.e("onFailure: ${message!!}")
                         }
 
                         override fun onStart() {
                             super.onStart()
-                            Log.e(TAG, "onStart: ")
+                            Timber.e("start :")
                         }
 
                         override fun onFinish() {
                             super.onFinish()
-                            listener?.getResult(filePath.toUri())
-                            Log.e(TAG, "onFinish: ")
+                            listener.getResult(filePath.toUri())
+                            Timber.e("finish!")
                         }
                     })
                 } catch (e: FFmpegCommandAlreadyRunningException) {
-                    listener?.onError(e.toString())
+                    listener.onError(e.toString())
                 }
             }
 
             override fun onFailure() {
-                Log.e("FFmpegLoad", "onFailure")
-                listener?.onError("Failed")
+                Timber.e("Load Failed")
+                listener.onError("Failed")
             }
 
             override fun onStart() {
             }
         })
-        listener?.onTrimStarted()
+        listener.onTrimStarted()
     }
 
 //    fun cropVideo(width: Int, height: Int, x: Int, y: Int, inputPath: String, outputPath: String, outputFileUri: Uri, listener: OnCropVideoListener?, frameCount: Int) {
